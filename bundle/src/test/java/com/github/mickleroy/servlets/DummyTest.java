343cc41c0ed029c59624441b0d28d02195cd22a0
Michael Leroy <mickjleroy@gmail.com>
2016-11-08 23:06:53 +1100

    started writing unit tests

---
+++ bundle/src/test/java/com/github/mickleroy/servlets/DummyTest.java
@@ -0,0 +1,13 @@
+
+package com.github.mickleroy.servlets;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void testSample() {
+        Assert.assertTrue(true);
+    }
+}
